42cd9e0a26fa342bc7c7ab5ca3e8c9f1eb6714cd
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-27 12:32:54 +0530

    [Nithin] Add :  Updation of Binarysearch

---
+++ DataStructuresAlgorithm/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAlgorithm
+{
+    public class BinarySearch
+    {
+        public void ReadTextFileAndSearch(string filePath)
+        {
+            string readAllText = File.ReadAllText(filePath);
+            string[] words = readAllText.Split(",");
+            Console.WriteLine("Enter key to Search");
+            string key = Console.ReadLine();
+            bool isFind = false;
+            foreach(var data in words)
+            {
+                if (data.Equals(key))
+                {
+                    isFind = true;
+                }
+            }
+            if(isFind)
+                Console.WriteLine("Search element {0} found", key);
+            else
+                Console.WriteLine("Search element not Found");
+        }
+    }
+}
---
+++ DataStructuresAlgorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DataStructuresAlgorithm
+{
+    class Program
+    {
+        static string searchFilePath = @"E:\BridgeLabz\DataStructureAlgorithm\DataStructuresAlgorithm\Search.txt";
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter option to get O/p :\n1.Binary Search\n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            while (flag)
+            {
+                switch(option)
+                {
+                    case 1:
+                        BinarySearch search = new BinarySearch();
+                        search.ReadTextFileAndSearch(searchFilePath);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter the correct i/p ");
+                        break;
+
+                }
+            }
+
+        }
+
+
+    }
+}
