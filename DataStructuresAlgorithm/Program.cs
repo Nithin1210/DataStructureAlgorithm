8e2805abda989f19d394f01ddad03f489a4d482d
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-28 19:16:02 +0530

    [Nithin] Add: Updation of PrimeNumberRange

--- DataStructuresAlgorithm/Program.cs
+++ DataStructuresAlgorithm/Program.cs
@@ -14,7 +14,7 @@ namespace DataStructuresAlgorithm
             while (flag)
             {
                 Console.WriteLine("\n Enter option to get O/p :\n1.Binary Search\n2.Anagram\n3.StringReplace \n4.Insertion sort \n5.Bubble sort " +
-                                    "\n6.PrimeNumberRange \n7.PrimePalandrome \n8.UnOrdered List \n9.Exit");
+                                    "\n6.PrimeNumberRange\n7.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -41,15 +41,6 @@ namespace DataStructuresAlgorithm
                     case 6:
                         PrimeNumberRange range = new PrimeNumberRange();
                         range.CheckPrime();
-                        break;
-                    case 7:
-                        PrimePalandrome prime = new PrimePalandrome();
-                        prime.CheckPrimePalandrome();
-                        break;
-
-                    case 8:
-                        Opertion unorderListProblem = new Opertion();
-                        unorderListProblem.ReadFileAndPerformOperation(unOrderedFilePath);
                         break;
                     case 9:
                         flag = false;
